b2a42de85a475e31f0f242675efd5ff337c50e56
Swapnil Raj <raj.swapnil99@gmail.com>
2022-03-16 01:15:23 +0530

    Initial implementation of generic TokenBridge

---
+++ contracts/src/ethereum/TokenBridge.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: Apache-2.0.
+pragma solidity ^0.6.12;
+
+import "@joriksch/sg-contracts/src/starkware/contracts/components/GenericGovernance.sol";
+import "@joriksch/sg-contracts/src/starkware/contracts/interfaces/ContractInitializer.sol";
+import "@joriksch/sg-contracts/src/starkware/contracts/interfaces/ProxySupport.sol";
+import "@joriksch/sg-contracts/src/starkware/cairo/eth/CairoConstants.sol";
+import "@joriksch/sg-contracts/src/starkware/starknet/eth/IStarknetMessaging.sol";
+import "@joriksch/sg-contracts/src/starkware/contracts/tokens/ERC20/IERC20.sol";
+
+contract TokenBridge is
+    GenericGovernance,
+    ContractInitializer,
+    ProxySupport
+{
+    event LogDeposit(address sender, address token, uint256 amount, uint256 l2Recipient);
+    event LogWithdrawal(address token, address recipient, uint256 amount);
+    event LogBridgeAdded(address l1Token, uint256 l2Token);
+
+    mapping(address => uint256) public l1TokentoL2Token;
+    IStarknetMessaging public messagingContract;
+    uint256 l2TokenBridge;
+
+    constructor() internal GenericGovernance("AAVE_BRIDGE_GOVERNANCE") {}
+
+    function isInitialized() internal view override returns (bool) {
+        return messagingContract != IStarknetMessaging(0);
+    }
+
+    function numOfSubContracts() internal pure override returns (uint256) {
+        return 0;
+    }
+
+    function validateInitData(bytes calldata data) internal pure override {
+        require(data.length == 64, "ILLEGAL_DATA_SIZE");
+    }
+
+    function processSubContractAddresses(bytes calldata subContractAddresses) internal override {}
+
+    modifier isApprovedToken(address token) {
+        uint256 l2TokenAddress = l1TokentoL2Token[token];
+        require(l2TokenAddress != 0, "L2_ADDRESS_OUT_OF_RANGE");
+        require(l2TokenAddress < CairoConstants.FIELD_PRIME, "L2_ADDRESS_OUT_OF_RANGE"); 
+        _;
+    }
+
+    /*
+      Gets the addresses of bridgedToken & messagingContract from the ProxySupport initialize(),
+      and sets the storage slot accordingly.
+    */
+    function initializeContractState(bytes calldata data) internal override {
+        (IStarknetMessaging messagingContract_, uint256 l2TokenBridge_) = abi.decode(
+            data,
+            (IStarknetMessaging, uint256)
+        );
+
+        require(l2TokenBridge_ != 0, "L2_ADDRESS_OUT_OF_RANGE");
+        require(l2TokenBridge_ < CairoConstants.FIELD_PRIME, "L2_ADDRESS_OUT_OF_RANGE");
+
+        messagingContract = messagingContract_;
+        l2TokenBridge = l2TokenBridge_;
+    }
+
+    // The selector of the deposit handler in L2.
+    // TODO: Recompute hash when the signature is decided
+    uint256 constant DEPOSIT_HANDLER =
+        1285101517810983806491589552491143496277809242732141897358598292095611420389;
+    uint256 constant TRANSFER_FROM_STARKNET = 0;
+    uint256 constant UINT256_PART_SIZE_BITS = 128;
+    uint256 constant UINT256_PART_SIZE = 2**UINT256_PART_SIZE_BITS;
+
+    modifier isValidL2Address(uint256 l2Address) {
+        require(l2Address != 0, "L2_ADDRESS_OUT_OF_RANGE");
+        require(l2Address < CairoConstants.FIELD_PRIME, "L2_ADDRESS_OUT_OF_RANGE");
+        _;
+    }
+
+    function approveBridge(address l1Token, uint256 l2Token)
+        external
+        onlyGovernance
+        isValidL2Address(l2Token)
+    {
+        require(l1Token != address(0x0), "l1Token address cannot be 0x0");
+
+        uint256 l2Token_ = l1TokentoL2Token[l1Token];
+        require(l2Token_ == 0, "l2Token already set");
+
+        emit LogBridgeAdded(l1Token, l2Token);
+        l1TokentoL2Token[l1Token] = l2Token;
+    }
+
+    function sendMessage(address l1Token, uint256 l2Recipient, uint256 amount)
+        internal
+        isApprovedToken(l1Token)
+        isValidL2Address(l2Recipient)
+    {
+        emit LogDeposit(msg.sender, l1Token, amount, l2Recipient);
+
+        uint256 l2TokenAddress = l1TokentoL2Token[l1Token];
+
+        uint256[] memory payload = new uint256[](5);
+        payload[0] = l2Recipient;
+        payload[1] = l2TokenAddress & (UINT256_PART_SIZE - 1);
+        payload[2] = l2TokenAddress >> (UINT256_PART_SIZE_BITS);
+        payload[3] = amount & (UINT256_PART_SIZE - 1);
+        payload[4] = amount >> UINT256_PART_SIZE_BITS;
+
+        messagingContract.sendMessageToL2(l2TokenBridge, DEPOSIT_HANDLER, payload);
+    }
+
+    function consumeMessage(address l1Token, address recipient, uint256 amount) internal {
+        emit LogWithdrawal(l1Token, recipient, amount);
+
+        uint256[] memory payload = new uint256[](5);
+        payload[0] = TRANSFER_FROM_STARKNET;
+        payload[1] = uint256(l1Token);
+        payload[1] = uint256(recipient);
+        payload[2] = amount & (UINT256_PART_SIZE - 1);
+        payload[3] = amount >> UINT256_PART_SIZE_BITS;
+
+        messagingContract.consumeMessageFromL2(l2TokenBridge, payload);
+    }
+
+    // TODO - USE MINT AND BURN FROM ERC20
+    function deposit(address l1Token_, uint256 l2Recipient, uint256 amount) external {
+        IERC20 l1Token = IERC20(l1Token_);
+        l1Token.transfer(address(this), amount);
+        sendMessage(l1Token_, l2Recipient, amount);
+    }
+
+    function withdraw(address l1Token_, address recipient, uint256 amount) isApprovedToken(l1Token_) external {
+        consumeMessage(l1Token_, recipient, amount);
+        require(recipient != address(0x0), "INVALID_RECIPIENT");
+        IERC20 l1Token = IERC20(l1Token_);
+
+        require(l1Token.balanceOf(msg.sender) - amount <= l1Token.balanceOf(msg.sender), "UNDERFLOW");
+        l1Token.transfer(recipient, amount);
+    }
+}
---
+++ contracts/src/starknet/TokenBridge.cairo
@@ -0,0 +1,165 @@
+%lang starknet
+
+from starkware.cairo.common.alloc import alloc
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+from starkware.cairo.common.math import assert_lt_felt, assert_not_zero
+from starkware.cairo.common.uint256 import Uint256
+from starkware.starknet.common.messages import send_message_to_l1
+from starkware.starknet.common.syscalls import get_caller_address, get_contract_address
+
+from src.starknet.IERC20 import IERC20
+
+const WITHDRAW_MESSAGE = 0
+const ETH_ADDRESS_BOUND = 2 ** 160
+
+# Storage.
+
+@storage_var
+func governor() -> (res : felt):
+end
+
+@storage_var
+func l1_token_bridge() -> (res : felt):
+end
+
+@storage_var
+func l2_token_to_l1_token(l2_token: felt) -> (l1_token: felt):
+end
+
+# Constructor.
+
+# To finish the init you have to initialize the L2 token contract and the L1 bridge contract.
+@constructor
+func constructor{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        governor_address : felt):
+    assert_not_zero(governor_address)
+    governor.write(value=governor_address)
+    return ()
+end
+
+# Getters.
+
+@view
+func get_governor{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}() -> (
+        res : felt):
+    let (res) = governor.read()
+    return (res)
+end
+
+@view
+func get_l1_token_bridge{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}() -> (
+        res : felt):
+    let (res) = l1_token_bridge.read()
+    return (res)
+end
+
+# Externals.
+
+@external
+func set_l1_token_bridge{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        l1_bridge_address : felt):
+    # The call is restricted to the governor.
+    let (caller_address) = get_caller_address()
+    let (governor_) = get_governor()
+    assert caller_address = governor_
+
+    # Check l1_bridge isn't already set.
+    let (l1_bridge_) = get_l1_token_bridge()
+    assert l1_bridge_ = 0
+
+    # Check new address is valid.
+    assert_lt_felt(l1_bridge_address, ETH_ADDRESS_BOUND)
+    assert_not_zero(l1_bridge_address)
+
+    # Set new value.
+    l1_token_bridge.write(value=l1_bridge_address)
+    return ()
+end
+
+@external
+func approve_bridge{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+    l1_token: felt, l2_token: felt):
+    # The call is restricted to the governor.
+    let (caller_address) = get_caller_address()
+    let (governor_) = get_governor()
+    assert caller_address = governor_
+
+    let (l1_token_) = l2_token_to_l1_token.read(l2_token)
+    with_attr error_message("L2 to L1 Bridge already setup"):
+        assert l1_token_ = 0
+    end
+
+    assert_not_zero(l2_token)
+    assert_not_zero(l1_token)
+    assert_lt_felt(l1_token, ETH_ADDRESS_BOUND)
+
+    l2_token_to_l1_token.write(l2_token, l1_token)
+    return ()
+end
+
+# TODO USE ERC20 MINT and BURN
+
+@external
+func initiate_withdraw{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        l2_token: felt, l1_recipient : felt, amount : Uint256):
+    # The amount is validated (i.e. amount.low, amount.high < 2**128) by an inner call to
+    # IMintableToken permissionedBurn function.
+
+    assert_not_zero(l2_token)
+
+    let (to_address) = get_l1_token_bridge()
+    # Check address is valid.
+    assert_lt_felt(to_address, ETH_ADDRESS_BOUND)
+    assert_not_zero(to_address)
+
+    # Check address is valid.
+    assert_lt_felt(l1_recipient, ETH_ADDRESS_BOUND)
+
+    let (l1_token) = l2_token_to_l1_token.read(l2_token)
+    with_attr error_message("L1 token not found"):
+        assert_not_zero(l1_token)
+    end
+
+    # Call burn on l2_token contract.
+    let (caller_address) = get_caller_address()
+    let (contract_address) = get_contract_address()
+
+    IERC20.transferFrom(
+        contract_address=l2_token, sender=caller_address, recipient=contract_address, amount=amount)
+
+    # Send the message.
+    let (message_payload : felt*) = alloc()
+    assert message_payload[0] = WITHDRAW_MESSAGE
+    assert message_payload[1] = l1_token
+    assert message_payload[2] = l1_recipient
+    assert message_payload[3] = amount.low
+    assert message_payload[4] = amount.high
+    
+    send_message_to_l1(to_address=to_address, payload_size=5, payload=message_payload)
+
+    return ()
+end
+
+@l1_handler
+func handle_deposit{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        from_address : felt, account : felt, l2_token_low: felt, l2_token_high: felt, amount_low : felt, amount_high : felt):
+    # The amount is validated (i.e. amount_low, amount_high < 2**128) by an inner call to
+    # IMintableToken permissionedMint function.
+
+    let (expected_from_address) = get_l1_token_bridge()
+    with_attr error_message("Expected deposit from l1_token_bridge"):
+        assert from_address = expected_from_address
+    end
+    let amount : Uint256 = cast((low=amount_low, high=amount_high), Uint256)
+
+    # Call mint on l2_token contract.
+    let l2_token_address = l2_token_high * 2**128 + l2_token_low
+
+    assert_not_zero(l2_token_address)
+
+    let (contract_address) = get_caller_address()
+    IERC20.transferFrom(
+        contract_address=l2_token_address, sender=contract_address, recipient=account, amount=amount)
+
+    return ()
+end

0da10776a0858d7eedf5aab69801097596760a09
Andrey Kozlov <kyzia.ru@gmail.com>
2022-05-06 17:26:00 +0200

    initial staticAtokenLM removal commit

--- contracts/src/l1/mocks/StaticATokenLMMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0.
-pragma solidity ^0.6.12;
-pragma experimental ABIEncoderV2;
-
-import {StaticATokenLM} from "@swp0x0/protocol-v2/contracts/protocol/tokenization/StaticATokenLM.sol";
-
-contract StaticATokenLMMock is StaticATokenLM {
-    constructor() public StaticATokenLM() {}
-}

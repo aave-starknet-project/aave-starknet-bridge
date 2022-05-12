14c4383ab09517dfadd6fc3c6d3d236d8d200e90
Jorik Schellekens <joriksch@gmail.com>
2022-05-12 12:38:24 +0100

    +1

--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -400,7 +400,7 @@ describe("Bridge", async function () {
     // check that tokens have been transfered to l1user
     // +1 to account for adai accrued since the deposit
     expect(await aDai.balanceOf(l1user.address)).to.equal(l1InitialADaiBalance + 1);
-    expect(await aUsdc.balanceOf(l1user.address)).to.equal(l1InitialAUsdcBalance);
+    expect(await aUsdc.balanceOf(l1user.address)).to.equal(l1InitialAUsdcBalance + 1);
     expect(await aDai.balanceOf(l1Bridge.address)).to.equal(30);
     expect(await aUsdc.balanceOf(l1Bridge.address)).to.equal(40);
 

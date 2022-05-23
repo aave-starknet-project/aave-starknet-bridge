068e6c50c198e745c73f711b8c2003966be3ca94
hsouf <soufianehajazi23@gmail.com>
2022-05-23 12:21:22 +0100

    comment code on Bridge.sol + clean up

--- contracts/l1/Bridge.sol
+++ contracts/l1/Bridge.sol
@@ -15,7 +15,6 @@ import {IScaledBalanceToken} from "@aave/protocol-v2/contracts/interfaces/IScale
 import {VersionedInitializable} from "@aave/protocol-v2/contracts/protocol/libraries/aave-upgradeability/VersionedInitializable.sol";
 
 contract Bridge is VersionedInitializable {
-    mapping(address => ATokenData) public aTokenData;
     IStarknetMessaging public messagingContract;
     uint256 public l2Bridge;
     address[] public approvedL1Tokens;
@@ -39,6 +38,7 @@ contract Bridge is VersionedInitializable {
         IERC20 underlyingAsset;
         ILendingPool lendingPool;
     }
+    mapping(address => ATokenData) public aTokenData;
 
     using SafeERC20 for IERC20;
     using WadRayMath for uint256;
@@ -57,7 +57,7 @@ contract Bridge is VersionedInitializable {
      * @dev Only bridge admin can call functions marked by this modifier.
      **/
     modifier onlyAdmin() {
-        require(msg.sender == admin, "ONLY ADMIN");
+        require(msg.sender == admin, "ONLY BRIDGE ADMIN");
         _;
     }
 
@@ -158,6 +158,148 @@ contract Bridge is VersionedInitializable {
         );
         approvedL1Tokens.push(l1AToken);
         emit LogTokenAdded(l1AToken, l2Token);
+    }
+
+    /**
+     * @notice allows deposits of aTokens or their underlying assets on L2
+     * @param l1AToken aToken address
+     * @param l2Recipient recipient address
+     * @param amount to be minted on l2
+     * @param referralCode of asset
+     * @param fromUnderlyingAsset if set to true will accept deposit from underlying assets
+     **/
+    function deposit(
+        address l1AToken,
+        uint256 l2Recipient,
+        uint256 amount,
+        uint16 referralCode,
+        bool fromUnderlyingAsset
+    ) external onlyValidL2Address(l2Recipient) returns (uint256) {
+        IERC20 underlyingAsset = aTokenData[l1AToken].underlyingAsset;
+        ILendingPool lendingPool = aTokenData[l1AToken].lendingPool;
+        require(
+            (underlyingAsset != IERC20(0x0)) &&
+                (lendingPool != ILendingPool(0x0)),
+            "This aToken has not been approved yet."
+        );
+
+        // deposit aToken or underlying asset
+
+        if (fromUnderlyingAsset) {
+            underlyingAsset.safeTransferFrom(msg.sender, address(this), amount);
+            lendingPool.deposit(
+                address(underlyingAsset),
+                amount,
+                address(this),
+                referralCode
+            );
+        } else {
+            IERC20(l1AToken).safeTransferFrom(
+                msg.sender,
+                address(this),
+                amount
+            );
+        }
+
+        // update L2 state and emit deposit event
+
+        uint256 rewardsIndex = getCurrentRewardsIndex(l1AToken);
+
+        uint256 staticAmount = _dynamicToStaticAmount(
+            amount,
+            address(underlyingAsset),
+            lendingPool
+        );
+
+        sendDepositMessage(
+            l1AToken,
+            msg.sender,
+            l2Recipient,
+            staticAmount,
+            block.number,
+            rewardsIndex
+        );
+
+        return staticAmount;
+    }
+
+    /**
+     * @notice allows withdraw of aTokens or their underlying assets from L2
+     * @param l1AToken aToken address
+     * @param l2sender sender address
+     * @param recipient l1 recipient
+     * @param staticAmount amount to be withdraw
+     * @param toUnderlyingAsset if set to true will withdraw underlying asset tokens from pool and transfer them to recipient
+     **/
+    function withdraw(
+        address l1AToken,
+        uint256 l2sender,
+        address recipient,
+        uint256 staticAmount,
+        uint256 l2RewardsIndex,
+        bool toUnderlyingAsset
+    ) external {
+        // check that the function call is valid and emit withdraw event
+
+        consumeMessage(
+            l1AToken,
+            l2sender,
+            recipient,
+            staticAmount,
+            l2RewardsIndex
+        );
+        require(recipient != address(0x0), "INVALID_RECIPIENT");
+
+        // withdraw tokens
+
+        address underlyingAsset = address(aTokenData[l1AToken].underlyingAsset);
+        ILendingPool lendingPool = aTokenData[l1AToken].lendingPool;
+        uint256 amount = _staticToDynamicAmount(
+            staticAmount,
+            underlyingAsset,
+            lendingPool
+        );
+
+        if (toUnderlyingAsset) {
+            lendingPool.withdraw(underlyingAsset, amount, recipient);
+        } else {
+            IERC20(l1AToken).safeTransfer(recipient, amount);
+        }
+
+        // update L2 state
+
+        uint256 l1CurrentRewardsIndex = getCurrentRewardsIndex(l1AToken);
+
+        sendIndexUpdateMessage(
+            l1AToken,
+            msg.sender,
+            block.number,
+            l1CurrentRewardsIndex
+        );
+
+        // transfer rewards
+
+        uint256 rewardsAmount = computeRewardsDiff(
+            staticAmount,
+            l2RewardsIndex,
+            l1CurrentRewardsIndex
+        );
+        transferRewards(recipient, rewardsAmount);
+    }
+
+    /**
+     * @notice allows bridge admin to manually update the rewards index of tokens on l2
+     * @param l1AToken aToken address
+     **/
+    function updateL2State(address l1AToken) external onlyAdmin {
+        uint256 rewardsIndex = getCurrentRewardsIndex(l1AToken);
+
+        sendIndexUpdateMessage(
+            l1AToken,
+            msg.sender,
+            block.number,
+            rewardsIndex
+        );
     }
 
     function sendDepositMessage(
@@ -283,136 +425,6 @@ contract Bridge is VersionedInitializable {
                 .add(index); // 18- precision, should be loaded
     }
 
-    function updateL2State(address l1AToken) external onlyAdmin {
-        uint256 rewardsIndex = getCurrentRewardsIndex(l1AToken);
-
-        sendIndexUpdateMessage(
-            l1AToken,
-            msg.sender,
-            block.number,
-            rewardsIndex
-        );
-    }
-
-    /**
-     * @notice allows deposits of aTokens or their underlying assets on L2
-     * @param l1AToken aToken address
-     * @param l2Recipient recipient address
-     * @param amount to be minted on l2
-     * @param referralCode of asset
-     * @param fromUnderlyingAsset if set to true will accept deposit from underlying assets
-     **/
-    function deposit(
-        address l1AToken,
-        uint256 l2Recipient,
-        uint256 amount,
-        uint16 referralCode,
-        bool fromUnderlyingAsset
-    ) external onlyValidL2Address(l2Recipient) returns (uint256) {
-        IERC20 underlyingAsset = aTokenData[l1AToken].underlyingAsset;
-        ILendingPool lendingPool = aTokenData[l1AToken].lendingPool;
-        require(
-            (underlyingAsset != IERC20(0x0)) &&
-                (lendingPool != ILendingPool(0x0)),
-            "This aToken has not been approved yet."
-        );
-
-        // deposit aToken or underlying asset
-
-        if (fromUnderlyingAsset) {
-            underlyingAsset.safeTransferFrom(msg.sender, address(this), amount);
-            lendingPool.deposit(
-                address(underlyingAsset),
-                amount,
-                address(this),
-                referralCode
-            );
-        } else {
-            IERC20(l1AToken).safeTransferFrom(
-                msg.sender,
-                address(this),
-                amount
-            );
-        }
-
-        // update L2 state and emit deposit event
-
-        uint256 rewardsIndex = getCurrentRewardsIndex(l1AToken);
-
-        uint256 staticAmount = _dynamicToStaticAmount(
-            amount,
-            address(underlyingAsset),
-            lendingPool
-        );
-
-        sendDepositMessage(
-            l1AToken,
-            msg.sender,
-            l2Recipient,
-            staticAmount,
-            block.number,
-            rewardsIndex
-        );
-
-        return staticAmount;
-    }
-
-    function withdraw(
-        address l1AToken,
-        uint256 l2sender,
-        address recipient,
-        uint256 staticAmount,
-        uint256 l2RewardsIndex,
-        bool toUnderlyingAsset
-    ) external {
-        // check that the function call is valid and emit withdraw event
-
-        consumeMessage(
-            l1AToken,
-            l2sender,
-            recipient,
-            staticAmount,
-            l2RewardsIndex
-        );
-        require(recipient != address(0x0), "INVALID_RECIPIENT");
-
-        // withdraw tokens
-
-        address underlyingAsset = address(aTokenData[l1AToken].underlyingAsset);
-        ILendingPool lendingPool = aTokenData[l1AToken].lendingPool;
-        uint256 amount = _staticToDynamicAmount(
-            staticAmount,
-            underlyingAsset,
-            lendingPool
-        );
-
-        if (toUnderlyingAsset) {
-            lendingPool.withdraw(underlyingAsset, amount, recipient);
-        } else {
-            IERC20(l1AToken).safeTransfer(recipient, amount);
-        }
-
-        // update L2 state
-
-        uint256 l1CurrentRewardsIndex = getCurrentRewardsIndex(l1AToken);
-
-        sendIndexUpdateMessage(
-            l1AToken,
-            msg.sender,
-            block.number,
-            l1CurrentRewardsIndex
-        );
-
-        // transfer rewards
-
-        uint256 rewardsAmount = computeRewardsDiff(
-            staticAmount,
-            l2RewardsIndex,
-            l1CurrentRewardsIndex
-        );
-        transferRewards(recipient, rewardsAmount);
-    }
-
     function computeRewardsDiff(
         uint256 amount,
         uint256 l2RewardsIndex,
@@ -440,6 +452,12 @@ contract Bridge is VersionedInitializable {
         messagingContract.consumeMessageFromL2(l2Bridge, payload);
     }
 
+    /**
+     * @notice called to transfer the bridged rewards tokens to the l1 recipient
+     * @param l2sender sender on l2
+     * @param recipient on l1
+     * @param amount of tokens to be claimed to user on l1
+     **/
     function receiveRewards(
         uint256 l2sender,
         address recipient,
@@ -450,6 +468,11 @@ contract Bridge is VersionedInitializable {
         transferRewards(recipient, amount);
     }
 
+    /**
+     * @notice claims pending rewards of the l1 bridge by calling the aave Incentives Controller and transfers them back to the l1 recipient
+     * @param recipient of rewards tokens
+     * @param rewardsAmount to be transferred to recipient
+     **/
     function transferRewards(address recipient, uint256 rewardsAmount)
         internal
     {

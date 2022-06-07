499388a883ff36cbffbe7d0a54975c945706071f
hsouf <soufianehajazi23@gmail.com>
2022-06-07 08:17:22 +0100

    no message

--- constants/allowlistedTokens.ts
+++
@@ -1,8 +0,0 @@
-//stores the metadata related to staticATokens to be deployed on l2
-export const allowlistedStaticATokens = [
-  { symbol: "saDAI", name: "staticADai", decimals: 18n },
-];
-//Stores addresesses of allowlisted aTokens to be approved on the l1 bridge at initialization
-export const allowlistedATokens = [
-  "0x028171bCA77440897B824Ca71D1c56caC55b68A3",
-];

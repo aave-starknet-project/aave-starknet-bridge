e59623eb5739f8e13163d8d0c22b04d8e1817fcc
massil <achab@users.noreply.github.com>
2022-10-16 22:54:36 +0200

    Clean repo (#117)
    * removed useless files
    
    * demete starknet artifacts

--- Makefile
+++
@@ -1,7 +0,0 @@
-.DEFAULT_GOAL := install
-
-install:
-	yarn
-	pip install poetry
-	poetry cache clear . --all
-	poetry install
--- scripts/static_analysis_solidity.sh
+++
@@ -1,8 +0,0 @@
-.venv/bin/slither . --exclude-low --exclude-medium --exclude-dependencies --exclude-optimization --sarif vulnerabilities/results_solidity.sarif
-
-if [ $? > 0 ]
-then
-	exit 0
-else
-	exit -1
-fi
